a081b25a4232b3d94b214e777fc3c16ffa5bd55e
Lee Pang (Contractor) <lpang@us.drwholdings.com>
2021-01-04 16:52:37 -0800

    Input search string, Google

--- sel.py
+++ sel.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 import time
 
 if __name__ == '__main__':
+    srch_str = input('Enter desired item to search for: ')
     driver = webdriver.Chrome('C:/Users/lpang/Code/chromedriver_win32/chromedriver')
-    driver.get('https://google.com')
+    driver.get('https://google.com/search?q={}'.format(srch_str))
     time.sleep(4)
     driver.quit()

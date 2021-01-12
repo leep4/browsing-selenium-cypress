dab634e0f746d876c163d75a89d9f01fd2dd09e6
Lee Pang (Contractor) <lpang@us.drwholdings.com>
2021-01-12 10:29:05 -0800

    Cypress - search and return site name, url to log

--- cyp/cypress/integration/google.spec.js
+++ cyp/cypress/integration/google.spec.js
@@ -1,13 +1,16 @@
+const SRCH_STR = 'casaba'
+
 describe('Test Search', () => {
   it('Visits Google', () => {
     cy.visit('https://google.com')
-    cy.get("[name='q']").type('casaba')
+    cy.get("[name='q']").type(SRCH_STR)
     cy.contains('Google Search').click()
     
     cy.get("[class='yuRUbf']").then(results => {
       for (let i = 0; i < results.length; i++) {
-        const txt = results.eq(i).text()
-        cy.log(txt)
+        const name = results.eq(i).find('h3').text()
+        const url = results.eq(i).find('a').attr('href')
+        cy.log(name, url)
       }
     })
   })
